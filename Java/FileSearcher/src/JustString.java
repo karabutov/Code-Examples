764f2ff77fcb6511aef4266b7df385a156678381
karabutov <32238762+karabutov@users.noreply.github.com>
2018-08-08 18:36:11 +0300

    Update JustString.java

--- Java/FileSearcher/src/JustString.java
+++ Java/FileSearcher/src/JustString.java
@@ -1,15 +1,17 @@
 package ru.karabutov.file_seacher.pattern;
 
+
 public class JustString extends PatternProcessor {
 
 	public JustString(String code) {
 		super(code);
 	}
 
+	
 	@Override
 	public boolean process(String str, State state) {
+		
 		int pos = str.indexOf(getCode(), state.getPosition());
-
 		if (pos < 0) {
 			return false;
 		}
@@ -17,8 +19,26 @@ public class JustString extends PatternProcessor {
 		if(!state.isAsteriksFaced() && pos != state.getPosition()) {
 			return false;
 		} 
-		state.setAsteriksFaced(false);
+		
 		state.setPosition(pos + getCode().length());
+		
+		if(state.isAsteriksFaced()) {
+			boolean ind = false;
+			while(!(ind = PatternContainer.getInstance().match(str.substring(state.getPosition(), str.length()), state.getPattern())) 
+																																																&&
+																																					state.getPosition()<str.length()) {
+				pos = str.indexOf(getCode(), state.getPosition());
+				if (pos < 0) {
+					return false;
+				}
+				state.setPosition(pos + getCode().length());
+			}		
+			state.setAsteriksFaced(false);
+			return ind;
+		}
+		
+		
+		state.setAsteriksFaced(false);
 		return true;
 	}
 
